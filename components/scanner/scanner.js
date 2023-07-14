a7dd5f13f184945199f6de533cb4626ea1959824
SachinthaLakshan <49986724+SachinthaLakshan@users.noreply.github.com>
2023-07-14 23:30:04 +0530

    wqqw

--- components/scanner/scanner.js
+++ components/scanner/scanner.js
@@ -1,100 +1,30 @@
-import { CameraEnhancer } from "dynamsoft-camera-enhancer";
-
-import { TextResult,BarcodeReader } from "dynamsoft-javascript-barcode";
-import React from "react";
+import React, { useState } from 'react';
+import { QrReader } from '@memenashi/react-qr-reader';
 
 
 
-const BarcodeScanner = (props) => {
-  const mounted = React.useRef(false);
-  const container = React.useRef(null);
-  const enhancer = React.useRef();
-  const reader = React.useRef();
-  const interval = React.useRef(null);
-  const decoding = React.useRef(false);
-  React.useEffect(()=>{
-    const init = async () => {
-      if (BarcodeReader.isWasmLoaded() === false) {
-        if (props.license) {
-          BarcodeReader.license = props.license;
-        }else{
-          BarcodeReader.license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==";
-        }
-        BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@9.6.11/dist/";
-      }
-      reader.current = await BarcodeReader.createInstance();
-      enhancer.current = await CameraEnhancer.createInstance();
-      //if(container.current!=null){
-        await enhancer.current.setUIElement(container.current);
-      //}
-      
-      enhancer.current.on("played", (playCallbackInfo) => {
-        if (props.onPlayed) {
-          props.onPlayed(playCallbackInfo);
-        }
-        startScanning();
-      });
-      enhancer.current.on("cameraClose", () => {
-        if (props.onClosed) {
-          props.onClosed();
-        }
-      });
-      enhancer.current.setVideoFit("cover");
-      if (props.onInitialized) {
-        props.onInitialized(enhancer.current,reader.current);
-      }
-      
-      toggleCamera();
-    }
-    if (mounted.current === false) {
-      init();
-    }
-    mounted.current = true;
-  },[])
-
-  const toggleCamera = () => {
-    if (mounted.current === true) {
-      if (props.isActive === true) {
-        enhancer.current?.open(true);
-      }else{
-        stopScanning();
-        enhancer.current?.close();
-      }
-    }
-  }
-
-  React.useEffect(()=>{
-    toggleCamera();
-  },[props.isActive])
-
-  const startScanning = () => {
-    const decode = async () => {
-      if (decoding.current === false && reader.current && enhancer.current) {
-        decoding.current = true;
-        const results = await reader.current.decode(enhancer.current.getFrame());
-        if (props.onScanned) {
-          props.onScanned(results);
-        }
-        decoding.current = false;
-      }
-    }
-    if (props.interval) {
-      interval.current = setInterval(decode,props.interval);
-    }else{
-      interval.current = setInterval(decode,40);
-    }
-  }
-
-  const stopScanning = () => {
-    clearInterval(interval.current);
-  }
-
-  return (
-    <div ref={container} style={{ position:"relative", width:"100%", height:"100%" }}>
-      <div className="dce-video-container"></div>
-      {props.children}
-    </div>
-  )
+const BarcodeScanner = () => {
+    const [error, setError] = useState('');
+    const [data, setData] = useState('');
+  
+    return (
+      <div >
+        <QrReader
+         
+          onResult={(result, error) => {
+            if (result) {
+              setData(result.getText());
+            }
+  
+            if (error) {
+              setError(error.message);
+            }
+          }}
+        />
+        <p>The value is: {JSON.stringify(data, null, 2)}</p>
+        <p>The error is: {error}</p>
+      </div>
+    );
 }
 
 export default BarcodeScanner;
--- src/pages/scan/index.js
+++ src/pages/scan/index.js
@@ -5,38 +5,14 @@ import BarcodeScanner from '../../../components/scanner/scanner';
 
 const ScanPage = () => {
 
-    const [isActive,setIsActive] = React.useState(false);
-  const [initialized,setInitialized] = React.useState(false);
-  const toggleScanning = () => {
-    setIsActive(!isActive);
-  }
-
-  const onScanned = (results) => {
-    if (results.length>0) {
-      let text = "";
-      for (let index = 0; index < results.length; index++) {
-        const result = results[index];
-        text = text + result.barcodeFormatString + ": " + result.barcodeText + "\n";
-      }
-      alert(text);
-      setIsActive(false);
-    }
-  }
+    
     return (
       <div className={homeStyles.app}>
       <h2>Next.js Barcode Scanner</h2>
-      {initialized ? (
-        <button onClick={toggleScanning}>{isActive ? "Stop Scanning" : "Start Scanning"}</button>
-      ) : (
-        <div>Initializing...</div>
-      )}
       <div className={homeStyles.barcodeScanner}>
         <BarcodeScanner
-          license={"DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9"}
-          onInitialized={() => setInitialized(true)}
-          isActive={isActive}
-          onScanned={(results) => onScanned(results)}
-        ></BarcodeScanner>
+          
+        />
       </div>
     </div>
     );
