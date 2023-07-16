176425d5862eabe1b7b50194f5deb2c26b3a8c5b
SachinthaLakshan <49986724+SachinthaLakshan@users.noreply.github.com>
2023-07-16 18:28:51 +0530

    dsf

--- components/scanner/scanner.js
+++ components/scanner/scanner.js
@@ -3,6 +3,7 @@ import QRScanner from "react-qr-scanner";
 
 const QRCodeReader = () => {
   const [data, setData] = useState("");
+  const [selectedCamera, setSelectedCamera] = useState(0);
 
   const handleScan = (event) => {
     const decodedData = event.detail.data;
@@ -14,17 +15,24 @@ const QRCodeReader = () => {
     console.error('QR Scanner Error:', error);
   };
 
+  
+
   return (
     <div>
+      <button onClick={() => setSelectedCamera(selectedCamera === 0 ? 1 : 0)}>
+        Switch Camera
+      </button>
       <QRScanner
         onScan={handleScan}
         onError={handleScanError}
+        facingMode={selectedCamera === 0 ? 'environment' : 'user'}
         style={{
           width: 200,
           height: 200,
         }}
       />
       <p>The decoded data is: {data}</p>
+      
     </div>
   );
 };
