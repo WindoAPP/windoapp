d613711e9ef172648211f0504e43360c438fab4d
SachinthaLakshan <49986724+SachinthaLakshan@users.noreply.github.com>
2023-07-16 18:35:58 +0530

    dsad

--- components/scanner/scanner.js
+++ components/scanner/scanner.js
@@ -1,38 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import QRScanner from "react-qr-scanner";
 
 const QRCodeReader = () => {
   const [data, setData] = useState("");
+  const [cameras, setCameras] = useState([]);
   const [selectedCamera, setSelectedCamera] = useState(0);
+  const scannerRef = useRef(null);
 
-  const handleScan = (event) => {
-    const decodedData = event.detail.data;
-    setData(decodedData);
-  };
+  useEffect(() => {
+    const fetchCameras = async () => {
+      try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === 'videoinput');
+        setCameras(cameras);
+      } catch (error) {
+        console.error("Error fetching cameras:", error);
+      }
+    };
+
+    fetchCameras();
+  }, []);
 
   const handleScanError = (error) => {
     // Handle the error here
     console.error('QR Scanner Error:', error);
   };
 
-  
+  const handleScan = (event) => {
+    const decodedData = event.detail.data;
+    setData(decodedData);
+  };
+
+  const switchCamera = () => {
+    setSelectedCamera(prevCamera => (prevCamera + 1) % cameras.length);
+  };
 
   return (
     <div>
-      <button onClick={() => setSelectedCamera(selectedCamera === 0 ? 1 : 0)}>
+      <button onClick={switchCamera}>
         Switch Camera
       </button>
       <QRScanner
         onScan={handleScan}
         onError={handleScanError}
-        facingMode={selectedCamera === 0 ? 'environment' : 'user'}
         style={{
           width: 200,
           height: 200,
         }}
+        cameraId={cameras[selectedCamera]?.deviceId}
+        ref={scannerRef}
       />
       <p>The decoded data is: {data}</p>
-      
     </div>
   );
 };
