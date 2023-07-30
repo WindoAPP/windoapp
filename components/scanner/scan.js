56b1ef604bb8454cf2943671dea2eb6703651bdb
SachinthaLakshan <49986724+SachinthaLakshan@users.noreply.github.com>
2023-07-31 00:35:03 +0530

    sadsad

--- components/scanner/scan.js
+++ components/scanner/scan.js
@@ -120,7 +120,7 @@ const Scan = () => {
     };
 
 
-    const [step, setStep] = useState(3);
+    const [step, setStep] = useState(0);
     const [price, setPrice] = useState();
 
     const onFinished = (winner) => {
