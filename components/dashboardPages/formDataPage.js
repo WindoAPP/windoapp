d9eba2f89593bb52303c3429ea5dad6feba2d018
SachinthaLakshan <49986724+SachinthaLakshan@users.noreply.github.com>
2023-08-12 17:25:06 +0530

    form edit

--- components/dashboardPages/formDataPage.js
+++ components/dashboardPages/formDataPage.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import styles from './profilePage.module.scss'
+import styles from './formDataPage.module.scss'
 import { useSession } from 'next-auth/react';
 import { getUser, updateUser } from '../../services/service';
 import Loader from '../Loader/loader';
@@ -23,7 +23,7 @@ const FormDataPage = () => {
                 setUser(res.user);
                 setIsLoading(false);
                 setFormData(
-                        { facebook: res.user.facebook ||"", instagram: res.user.instagram ||"", email: res.user.email, password: '', c_password: '', userName: res.user.userName, phoneNumber: res.user.phoneNumber, shopName: res.user.shopName, shopId: res.user.shopId }
+                    { facebook: res.user.facebook || "", instagram: res.user.instagram || "", email: res.user.email, password: '', c_password: '', userName: res.user.userName, phoneNumber: res.user.phoneNumber, shopName: res.user.shopName, shopId: res.user.shopId }
                 )
             }
         }).catch(err => {
@@ -39,19 +39,19 @@ const FormDataPage = () => {
     const registerFromSubmit = (e) => {
         e.preventDefault();
         var userData = user;
-        
-        if(formData.password!="" || formData.c_password!=""){
-            if(formData.password===formData.c_password){
-                userData.password=formData.password;
-            }else{
+
+        if (formData.password != "" || formData.c_password != "") {
+            if (formData.password === formData.c_password) {
+                userData.password = formData.password;
+            } else {
                 return showNotification(true, "password not matched")
             }
         }
 
-        userData.email=formData.email;
-        userData.userName=formData.userName
-        userData.phoneNumber=formData.phoneNumber
-        userData.shopName=formData.shopName
+        userData.email = formData.email;
+        userData.userName = formData.userName
+        userData.phoneNumber = formData.phoneNumber
+        userData.shopName = formData.shopName
         userData.shopId = formData.shopId
         userData['facebook'] = formData.facebook;
         userData['instagram'] = formData.instagram;
@@ -70,9 +70,9 @@ const FormDataPage = () => {
         <>
             {
                 !isLoading ? <div className={`p-4 d-flex flex-column align-items-center justify-content-center ${styles.mainContent}`}>
-                    <div>
-                        <h1 >Add your social network links</h1>
-                        <p className='text-muted'>By including this you can increase your number of followers </p>
+                    <div className={`${styles.formWrapper}`}>
+                        {/* <h1 >Add your social network links</h1>
+                        <p className='text-muted'>By including this you can increase your number of followers </p> */}
                         <form className='d-flex flex-column'>
                             <div className="d-flex flex-column">
                                 <label><strong>User Name</strong></label>
@@ -82,37 +82,45 @@ const FormDataPage = () => {
                                 <label><strong>Email</strong></label>
                                 <input required type="email" name="email" placeholder="Email" className="form-control regi-input" onChange={handleChange} value={formData.email}></input>
                             </div>
-                            <div className="d-flex flex-column">
-                                <label><strong>Password</strong></label>
-                                <input required type="password" name="password" placeholder="••••••••••" className="form-control regi-input" onChange={handleChange} value={formData.password}></input>
-                            </div>
-                            <div className="d-flex flex-column">
-                                <label><strong>Confirm Password</strong></label>
-                                <input type="password" name="c_password" placeholder="••••••••••" className="form-control regi-input" onChange={handleChange} value={formData.c_password}></input>
-                            </div>
-                            <div className="d-flex flex-column">
-                                <label><strong>Shop Name</strong></label>
-                                <input type="text" name="shopName" placeholder="Shop Name" className="form-control regi-input" onChange={handleChange} value={formData.shopName}></input>
-                            </div>
-                            <div className="d-flex flex-column">
-                                <label><strong>Mobile Number</strong></label>
-                                <input type="text" name="phoneNumber" placeholder="Mobile Number" className="form-control regi-input" onChange={handleChange} value={formData.phoneNumber}></input>
-                            </div>
-                            <div className="d-flex flex-column">
-                                <div className='d-flex flex-row'>
-                                    <label><strong>shop ID</strong></label>
-                                    <label className='mx-2'><a target="_blank" href='https://ultimateelementor.com/docs/find-google-place-id/'>click here</a> to get your shop ID </label>
+                            <div className='d-flex flex-row w-100'>
+                                <div className="d-flex flex-column w-100 mr-2" >
+                                    <label><strong>Password</strong></label>
+                                    <input required type="password" name="password" placeholder="••••••••••" className="form-control regi-input" onChange={handleChange} value={formData.password}></input>
                                 </div>
+                                <div className="d-flex flex-column w-100">
+                                    <label><strong>Confirm Password</strong></label>
+                                    <input type="password" name="c_password" placeholder="••••••••••" className="form-control regi-input" onChange={handleChange} value={formData.c_password}></input>
+                                </div>
+                            </div>
+                            <div className='d-flex flex-row w-100'>
+                                <div className="d-flex flex-column w-100 mr-2">
+                                    <label><strong>Shop Name</strong></label>
+                                    <input type="text" name="shopName" placeholder="Shop Name" className="form-control regi-input" onChange={handleChange} value={formData.shopName}></input>
+                                </div>
+                                <div className="d-flex flex-column w-100">
+                                    <label><strong>Mobile Number</strong></label>
+                                    <input type="text" name="phoneNumber" placeholder="Mobile Number" className="form-control regi-input" onChange={handleChange} value={formData.phoneNumber}></input>
+                                </div>
+                            </div>
 
-                                <input type="text" name="shopId" placeholder="shop ID" className="form-control regi-input " onChange={handleChange} value={formData.shopId}></input>
-                            </div>
-                            <div className="form-group my-2">
-                            <label><strong>Facebook link</strong></label>
-                                <input type="text" className="form-control" name="facebook" placeholder="Enter your facebook link" value={formData.facebook} onChange={handleChange}></input>
-                            </div>
-                            <div className="form-group my-2">
-                            <label><strong>Instagram link</strong></label>
-                                <input type="text" className="form-control" name="instagram" placeholder="Enter your instagram link" value={formData.instagram} onChange={handleChange}></input>
+                            <div className='card p-3 mt-5'>
+                                <h5 className={styles.socialLinkTitle}>CHANGEMENT DE MOT DE PASSE</h5>
+                                <div className="d-flex flex-column mt-3">
+                                    <div className='d-flex flex-row'>
+                                        <label><strong>shop ID</strong></label>
+                                        <label className='mx-2'><a target="_blank" href='https://ultimateelementor.com/docs/find-google-place-id/'>click here</a> to get your shop ID </label>
+                                    </div>
+
+                                    <input type="text" name="shopId" placeholder="shop ID" className="form-control regi-input " onChange={handleChange} value={formData.shopId}></input>
+                                </div>
+                                <div className="form-group my-2">
+                                    <label><strong>Facebook link</strong></label>
+                                    <input type="text" className="form-control" name="facebook" placeholder="Enter your facebook link" value={formData.facebook} onChange={handleChange}></input>
+                                </div>
+                                <div className="form-group my-2">
+                                    <label><strong>Instagram link</strong></label>
+                                    <input type="text" className="form-control" name="instagram" placeholder="Enter your instagram link" value={formData.instagram} onChange={handleChange}></input>
+                                </div>
                             </div>
 
                             <button onClick={registerFromSubmit} className="btn btn-success mt-2">Save</button>
