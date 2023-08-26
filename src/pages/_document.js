71f1f50e2488b2547aab8d8fadc4812f705d8ae5
SachinthaLakshan <49986724+SachinthaLakshan@users.noreply.github.com>
2023-08-27 04:01:41 +0530

    title icon issue fixed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head >
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+      <link rel="icon" href="/title.ico" />
       </Head>
       <body>
         <Main />
