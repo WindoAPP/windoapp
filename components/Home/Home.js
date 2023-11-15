72e1318d789b0d12a4cd760506496def1b1585a3
SachinthaLakshan <sachintha.kahagalla@gmail.com>
2023-11-15 22:28:50 +0530

    button color fixed

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -288,7 +288,7 @@ const Home = () => {
           <center>
             <div className="row">
               <div className="col">
-                <button className="btn btn-warning font-weight-bold" >ENVOYER</button>
+                <button className="btn btn-success font-weight-bold" >ENVOYER</button>
               </div>
             </div>
           </center>
